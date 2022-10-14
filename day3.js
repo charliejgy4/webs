31679be439b8570cca12ee0c7e7568d8a6f21e14
charliejgy4 <103393876+charliejgy4@users.noreply.github.com>
2022-10-14 11:11:38 +0100

    Add files via upload

---
+++ day3.js
@@ -0,0 +1,16 @@
+let age = 18
+let country ="UK"
+if (age >17 && country == "UK"){
+    console.log ("yes i can serve you") 
+
+} { else if (age >=21 && country == "USA" )
+
+} {console.log ("Yes i can serve you")
+    
+ }
+ { console.log ("get out")
+
+}
+
+  
+    
